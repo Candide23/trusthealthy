bf4db2d248ac86ac07559a4549f9841df121ceab
candide Mboungou-Kimpolo <mboungoukimpolo@gmail.com>
2022-11-18 10:11:03 -0600

    first commit

---
+++ src/main/java/com/example/demo/TrustHealthyApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrustHealthyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrustHealthyApplication.class, args);
+	}
+
+}
